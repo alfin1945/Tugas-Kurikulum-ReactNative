90ba1ae58abb64dcd02ef592ca172aff175bfd7c
alfin1945 <alfinmardani7@gmail.com>
2020-09-17 10:16:13 +0700

    Tambahkan file remidi soal3 sprint3

---
+++ Tugas/Remidi/soal3/index.js
@@ -0,0 +1,8 @@
+function LuasPersegiPanjang(panjang, lebar) {
+  var total = panjang * lebar;
+  document.write("<b>ket:</b><br>panjang:  <10><br>");
+  document.write("lebar: <5><br><br><br>");
+  document.write("<b>Luas Persegi Panajang:</b> <" + total + ">");
+}
+
+LuasPersegiPanjang(10, 5);
