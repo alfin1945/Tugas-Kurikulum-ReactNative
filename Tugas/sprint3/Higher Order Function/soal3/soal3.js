5372ee06cc1c12ccf1fd3cb3eca649137042ce59
alfin1945 <alfinmardani7@gmail.com>
2020-09-17 22:01:00 +0700

    Tambahkan file tugas sprint3

---
+++ Tugas/sprint3/Higher Order Function/soal3/soal3.js
@@ -0,0 +1,26 @@
+var kodeUnix = [
+  23,
+  89,
+  67,
+  29,
+  192,
+  6,
+  2,
+  129,
+  21,
+  872,
+  891,
+  901,
+  70,
+  61,
+  78,
+  62,
+  32,
+  90,
+  90,
+];
+
+var banyakNilai = kodeUnix.length;
+const kode = kodeUnix.map((a) => (a * 2) / 5 - banyakNilai);
+
+console.log(kode);
