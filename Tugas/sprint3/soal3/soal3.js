d6416b9fa9c07384d898ac6d2e92cee71ce531d1
alfin1945 <alfinmardani7@gmail.com>
2020-09-17 10:10:43 +0700

    Tambahkan file soal3 sprint3

---
+++ Tugas/sprint3/soal3/soal3.js
@@ -0,0 +1,6 @@
+const person = { name: "rizal", asal: "Prabumulih" }; // jadi disini terdapat kurung kurawal yg dimana ini bukan salah satu ciri dari array akan tetapi kurung kurawal ciri dari objek yg saya ketahui
+
+var { name, asal } = person;
+
+console.log(name);
+console.log(asal);
