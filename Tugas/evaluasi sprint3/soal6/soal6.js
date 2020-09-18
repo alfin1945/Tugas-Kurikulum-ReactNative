aa8295e5f87805ac7a3a885bf1e385662404ddaf
alfin1945 <alfinmardani7@gmail.com>
2020-09-18 21:33:14 +0700

    Tambahkan evaluasi sprint3

---
+++ Tugas/evaluasi sprint3/soal6/soal6.js
@@ -0,0 +1,11 @@
+function mentorProgrammer(nama, status, asal) {
+  console.log(nama);
+  console.log(status);
+  console.log(asal);
+}
+
+mentorProgrammer(
+  "Nama Mentor Wandi Pratama",
+  "Statusnya nya Santri",
+  "Berasal dari kota Prabumulih"
+);
