7e034376a7793fbcd2bfe4765de39680f89cf9f1
alfin1945 <alfinmardani7@gmail.com>
2020-09-26 13:31:36 +0700

    Tambahkan evalusi sprint4

---
+++ Tugas/sprint4/soal4/soal4.js
@@ -0,0 +1,17 @@
+function  mentorProgrammer(nama,status,asal){
+    nama()
+    status()
+    asal()
+
+}
+function nama() {
+    console.log("Nama mentor Wandi Pratama");
+}
+function status() {
+    console.log("Status nya adalah Santri");
+}
+function asal() {
+    console.log("Berasal dari kota Prabumulih");
+}
+
+mentorProgrammer(nama,status,asal)
