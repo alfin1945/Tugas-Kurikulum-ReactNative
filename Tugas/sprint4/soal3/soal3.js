c974fcf1599df2e651ad76c05bddac4bcc0624ce
alfin1945 <alfinmardani7@gmail.com>
2020-09-26 13:38:54 +0700

    Tambahkan evaluasi sprint4

---
+++ Tugas/sprint4/soal3/soal3.js
@@ -0,0 +1,7 @@
+function LuasPersegiPanjang(panjang,lebar){
+    var total = panjang * lebar
+    document.write("panjang: <5><br>")
+    document.write("lebar:  <4><br><br>")
+    document.write("LuasPersegiPanjang: <"+total+">")
+}
+LuasPersegiPanjang(5,4)
