a83e51d951ef103f4f1bdb3abeec4951ea93d9ae
alfin1945 <alfinmardani7@gmail.com>
2020-10-03 14:26:29 +0700

    Tambahkan soal2

--- Tugas/remidial akhir/soal2/soal2.js
+++ Tugas/remidial akhir/soal2/soal2.js
@@ -2,7 +2,7 @@ var nilaiSantri = [90, 60, 65, 97, 99, 60, 53, 91, 89];
 
 var total = 0;
 
-for (i in nilaiSantri) {
+for (i = 0; i < nilaiSantri.length; i++) {
   console.log(i);
   total += nilaiSantri[i];
 }
